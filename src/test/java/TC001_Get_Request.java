7af9a961e434a66b52448e0242302369bdb8d3f0
OKC QE <69338152+OKC-QE@users.noreply.github.com>
2022-03-03 13:07:24 +0530

    Rest Assured Project

---
+++ src/test/java/TC001_Get_Request.java
@@ -0,0 +1,41 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TC001_Get_Request {
+
+	@Test
+	void getWeatherDetails() {
+		
+		//Specify the base URI
+		RestAssured.baseURI = "https://demoqa.com/utilities/wheather/city";
+		
+		//Request Object
+		RequestSpecification hhtpRequest = RestAssured.given();
+		
+		//Response object
+		Response response = hhtpRequest.request(Method.GET,"/Hyderabad");
+		
+		
+		//Printing response body in console
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body: "+ responseBody);
+		
+		
+		//status code validation
+		int statusCode = response.getStatusCode();	
+		System.out.println("Response code:" + statusCode);
+		
+		Assert.assertEquals(statusCode, 201);
+		
+		//status line validation
+		String statusLine = response.getStatusLine();
+		Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+		
+		}
+
+}
