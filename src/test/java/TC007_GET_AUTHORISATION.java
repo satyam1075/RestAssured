8ba1bc08db5be68e5e1d2ed3c1aa8f4e91a1c947
Satyam Roy <satyamroy1075@gmail.com>
2022-03-07 10:44:02 +0530

    Update TC007_GET_AUTHORISATION.java
    New

--- src/test/java/TC007_GET_AUTHORISATION.java
+++ src/test/java/TC007_GET_AUTHORISATION.java
@@ -1,4 +1,42 @@
+import org.testng.Assert;
+
+import com.google.gson.Gson;
+
+import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class TC007_GET_AUTHORISATION {
 
+	void authorisationTest() {
+
+		// Specify the base URI
+		RestAssured.baseURI = "https://reqres.in/api/users";
+
+		// Authentication UPI
+		PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
+		authScheme.setUserName("Something");
+		authScheme.setPassword("Password");
+
+		RestAssured.authentication = authScheme;
+
+		// Request Object
+		RequestSpecification hhtpRequest = RestAssured.given();
+
+		// Response object
+		Response response = hhtpRequest.request(Method.GET, "/2");
+
+		// status code validation
+		int statusCode = response.getStatusCode();
+		System.out.println("Response code:" + statusCode);
+		Assert.assertEquals(statusCode, 200);
+
+		// Printing response body in console
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body: " + responseBody);
+
+	}
 }
